1c2067da00c9340c44388f5636151fe1a0759c50
ayberkmeral <meraldursunayberk@gmail.com>
2025-02-28 18:50:05 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int num1;
+cout<<"enter a number: ";
+cin>>num1;
+
+int num2;
+cout<<"enter a number: ";
+cin>>num2;
+
+int sum=num1+num2;
+
+cout<<num1<<"+"<<num2<<"="<<sum<<endl;
+
+
+
+
+    return 0;
+}
