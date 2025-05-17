f547131948ec2b794768e320eca6dd821b404a94
ayberkmeral <meraldursunayberk@gmail.com>
2025-05-17 10:38:28 +0300

    Monthly Salary Program in C++
    Monthly Salary Program in C++ using classes

--- main.cpp
+++ main.cpp
@@ -1,64 +1,60 @@
-
 #include <iostream>
-
-#include <iomanip>
+#include<iomanip>
 
 using namespace std;
 
-int main() {
+class salary{
+public:
+    salary();
+    void set_salary();
+    void print();
 
-int a = 2;
+private:
+    bool check(int);
+    int months[12];
+    int total;
+};
 
-char b = 'f';
+salary::salary(){
+for(int i=0;i<12;i++)
+    months[i]=0;
+total=0;
+}
 
-float c = 3.141592f;
+bool salary::check(int value){
+return value>=0?true:false;
+}
 
-double d = 3;
+void salary::set_salary(){
+int amount;
 
-cout << setw(3) << a << endl;
+for(int i=0;i<12;i++){
+    cout<<"enter "<<i+1<<". salary";
+    do{
+        cin>>amount;
+    }while(!check(amount));
 
-//Sets the field width to 3 characters
-
-//setw(n) sets the width of the next output field to n characters.
-
-cout << setw(3) << left << a << endl;
-
-//print an integer within 3 spaces which is left aligned
-
-//setw() right:This means that the value is aligned to the right, and any extra spaces are added to the left.
-
-//setw() left:This aligns the value to the left, and any extra spaces are added to the right.
-
-cout << setw(3) << a << endl;
-
-//print an integer within 3 spaces which is right aligned
-
-cout << '\t' << a << '\t' << b << '\t' << c << endl;
-
-//print  horizontal tab character between a and b and c
-
-cout << setw(9) << a << setw(8) << b << setw(13) << c << endl;
-
-//It locates 9 spaces for a 8 for b and 13 for c
-
-cout << d << '\t' << setprecision(1) << d << '\t' << fixed << setprecision(1) << d << endl;
-
-//setprecision(n), it forces the output to display exactly n decimal places
-
-// Fixed is not used thus number of digits will be the total number of digits.
-
-//The third d is printed with one digit after the decimal point, because fixed is enabled.
-
-cout.unsetf(ios::fixed);
-
-// unsetf Removes the fixed formatting, restoring the default floating-point notation.
-
-cout << d << endl;
-
-//it is printed default
+months[i]=amount;
+total+=months[i];
+}
+}
 
 
+void salary::print(){
+cout<<"monthly salaries"<<endl;
 
-return 0;
+for(int i=0;i<12;i++)
+    cout<<setw(2)<<i+1<<". month"<<months[i]<<endl;
+cout<<"ur total is "<<total<<endl;
+}
+int main()
+{
 
+salary s;
+
+s.set_salary();
+s.print();
+
+
+    return 0;
 }
